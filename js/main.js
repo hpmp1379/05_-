0dbdff361788f27e7c3c2305f9348092a499deee
Min Park <88412085+hpmp1379@users.noreply.github.com>
2022-09-20 11:12:07 +0900

    0920_fin

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
-$(document).ready(function(){
-    $('.header_in a').click(function(){
-        $(this).toggleClass('on');
-    })
+// $(document).ready(function(){ //without defer attr in script tag
+//     $('.header_in a').click(function(){
+//         $(this).toggleClass('on');
+//     })
+// })
+
+// add attr defer to <script> in html
+$('.trigger').click(function(){
+    $(this).toggleClass('on');
+    $('nav').slideToggle(400);
+})
+$(window).resize(function(){
+    let winW = $(window).width(); //브라우저의 가로길이를 변수에
+    console.log(winW);
+
+    if(winW >= 768 && $('nav').is(':hidden')){
+        console.log('nav 안보여요!')
+        $('nav').removeAttr('style');
+    }
 })
